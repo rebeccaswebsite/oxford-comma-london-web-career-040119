a86b0d97f0c555be5db40726207039dff7df1f45
rebeccaswebsite <rebecca.huseyin.14@ucl.ac.uk>
2019-03-15 15:35:21 +0000

    added method to turn an array into string

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+	if array.length == 1
+		array.join
+	elsif array.length == 2
+	array.insert(-2, "and")
+	array.join(" ")
+	elsif array.length > 2
+	array[0..-2].join(", ") + ", and " + array[-1]
+	end
 end
+
